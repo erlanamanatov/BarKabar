2ba7ba55e2c84508dad3f12fba51e80e99767870
erlanamanatov <erlanamanatov@gmail.com>
2018-11-20 10:53:43 +0600

    Add locale to SimpleDateFormat formatter to fix bug in russian  language devices
    *change color of progress bar for sputnik fragment and kloop fragment

--- app/src/main/java/com/erkprog/barkabar/util/DateFormatter.java
+++ app/src/main/java/com/erkprog/barkabar/util/DateFormatter.java
@@ -1,17 +1,22 @@
 package com.erkprog.barkabar.util;
 
 
+import android.util.Log;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateFormatter {
+  private static final String TAG = "DateFormatter";
 
-  public static Date getDate(String stringDate) {
+  private static Date getDate(String stringDate) {
     if (stringDate == null) {
       throw new IllegalArgumentException("Parsing error");
     }
-    SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM y H:mm:ss Z");
+    Locale locale = new Locale("en");
+    SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM y H:mm:ss Z", locale);
     try {
       return formatter.parse(stringDate);
     } catch (ParseException e) {
@@ -20,9 +25,8 @@ public class DateFormatter {
   }
 
   public static String getFormattedDate(String resourceDate) throws IllegalArgumentException {
-
+    Log.d(TAG, "getFormattedDate: resource date " + resourceDate);
     SimpleDateFormat formatter = new SimpleDateFormat("d/MM/yy H:mm");
-
     Date date = getDate(resourceDate);
     return formatter.format(date);
 
